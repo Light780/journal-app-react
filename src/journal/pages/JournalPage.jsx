04a72c75b5ba3d8ed72bc115309933066cad9c66
Bruno Ramos <70899175+Light780@users.noreply.github.com>
2023-01-10 20:39:16 -0500

    Layout terminado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,24 @@
-import { Typography } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../views'
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1' component='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <NothingSelectedView />
+
+      <IconButton
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+    </JournalLayout>
   )
 }
---
+++ src/ui/components/SideBar.jsx
@@ -0,0 +1,41 @@
+import { TurnedInNot } from '@mui/icons-material'
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
+
+export const SideBar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component='nav'
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant='permanent'
+        open
+        sx={{
+          display: { xs: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+        }}
+      >
+        <Toolbar>
+          <Typography variant='h6' noWrap component='div'>Bruno Ramos</Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {
+            ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <TurnedInNot />
+                    <Grid container>
+                      <ListItemText primary={text} />
+                    </Grid>
+                  </ListItemIcon>
+                </ListItemButton>
+              </ListItem>
+            ))
+          }
+        </List>
+      </Drawer>
+    </Box>
+  )
+}
